6eaf4df22e45c31ef7f64cdaf6ede9a42df74fb3
ADITYA IMAM ZUHDI <onlykagaya@gmail.com>
2023-08-16 10:14:00 +0700

    Artikel update

---
+++ resources/views/layout/artikel.blade.php
@@ -0,0 +1,360 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src=“https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js”></script>
+    <script src=“https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js”></script>
+    <script src=“https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js”></script>
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+
+    
+    <!-- My Style -->
+   
+    <!-- Logo Title Bar -->
+    <link rel="icon" href="Assets/png/logo-persagi.png"
+    type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+   
+    <title>Persagi</title>
+    <style>
+      html,
+      body {
+        position: relative;
+        height: 100%;
+      }
+  
+      body {
+        background: #eee;
+        font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+        font-size: 14px;
+        color: #000;
+        margin: 0;
+        padding: 0;
+      }
+  
+      .swiper {
+        width: 100%;
+        height: 100%;
+      }
+  
+      .swiper-slide {
+        text-align: center;
+        font-size: 18px;
+        background: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+  
+      .swiper-slide img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+  
+      .swiper-slide {
+        width: 80%;
+      }
+  
+      .swiper-slide:nth-child(2n) {
+        width: 60%;
+      }
+  
+      .swiper-slide:nth-child(3n) {
+        width: 40%;
+      }
+    </style>
+  </head>
+
+  <body>
+      <!--Navbar-->
+      <div class="nav-top bg-main py-2">
+        <div class="container d-flex justify-content-between">
+            <p class="my-auto fw-300">Senin, 07 Agustus 2023, 21:30 WIB</p>
+            <button class="btn btn-main px-3" type="button">
+                Masuk
+            </button>
+        </div>
+      </div>
+      <div class="nav-mid bg-white py-3">
+        <img src="Assets/png/logo-persagi.png" alt="" class="img-fluid d-block mx-auto">
+        <p class="text-center text-main">“ Dorong kesejahteraan gizi, dengan dukungan PERSAGI ”</p>
+    </div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-main sticky-top">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse txt-nav " id="navbarNavAltMarkup">
+            <ul class="navbar-nav mx-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Layanan</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Artikel</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Event</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Kontak</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <!-- Artikel Populer -->
+      <section class="" style="background-color: #E1F3FF;">
+        <div class="container py-5">
+            <div class=" text-left py-5 d-flex" style="margin:  0 auto;">
+              <h1>
+                Artikel <span class="text-title">Populer</span>
+              </h1>
+            </div>
+            <div class="d-flex flex-column gap-3">
+              <div class="card d-flex gap-3 card-lg" style="height: 260px; width: 1167px; margin: 0 auto;">
+                    <div class="row">
+                      <div class="col-4 d-flex align-items-center">
+                          <img src="Assets/png/23 1.png" style="width: 355px; height: 240px; margin-left: 15px; margin-bottom: 12px;" class="d-flex align-items-center">
+                        </div>
+                        <div class="col">
+                          <div class="pt-3">
+                              <h1 class="text-artikel">Pentingnya Gizi: Menjaga Kesehatan dan Produktivitas dalam Profesi</h1>
+                              <div class="d-flex gap-2">
+                                  <p class="txt-mid-art py-2">KARAWANG - Gizi yang seimbang dan berkualitas bukan hanya sekadar tren, tetapi juga merupakan pilar utama dalam menjaga kesehatan dan meningkatkan produktivitas dalam kehidupan sehari-hari. DPC Persagi Karawang dengan tegas mengangkat isu pentingnya gizi bagi anggota dan komunitas dalam upaya mempromosikan kesejahteraan dan kemampuan kerja yang optimal.</p>
+                              </div>
+                              <div class="d-flex justify-content-between py-3 ms-auto">
+                                  <p class="txt-foot-art d-flex gap-3 "><img src="Assets/png/Vector (5).png"  style ="width: 26px; height: 23.636px;" alt=""> 21 Februari 2021</p>
+                                  <p></p>
+                                  <a href="#" class="btn btn-out" style="margin-right: 20px;">Baca Selengkapnya</a>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+                <div class="card d-flex gap-3 card-lg" style="height: 260px; width: 1167px; margin: 0 auto;">
+                  <div class="row">
+                      <div class="col-4 d-flex align-items-center">
+                          <img src="Assets/png/22 1.png" style="width: 355px; height: 240px; margin-left: 15px; margin-bottom: 12px;" class="d-flex align-items-center">
+                      </div>
+
+
+                      <div class="col">
+                          <div class="pt-3">
+                              <h1 class="text-artikel">Pentingnya Gizi: Menjaga Kesehatan dan Produktivitas dalam Profesi</h1>
+                              <div class="d-flex gap-2">
+                                  <p class="txt-mid-art py-2">KARAWANG - Gizi yang seimbang dan berkualitas bukan hanya sekadar tren, tetapi juga merupakan pilar utama dalam menjaga kesehatan dan meningkatkan produktivitas dalam kehidupan sehari-hari. DPC Persagi Karawang dengan tegas mengangkat isu pentingnya gizi bagi anggota dan komunitas dalam upaya mempromosikan kesejahteraan dan kemampuan kerja yang optimal.</p>
+                              </div>
+                              <div class="d-flex justify-content-between py-3 ms-auto">
+                                <p class="txt-foot-art d-flex gap-3 "><img src="Assets/png/Vector (5).png"  style ="width: 26px; height: 23.636px;" alt=""> 21 Februari 2021</p>
+                                <p></p>
+                                <a href="#" class="btn btn-out" style="margin-right: 20px;">Baca Selengkapnya</a>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              
+
+              <div class="card d-flex gap-3 card-lg" style="height: 260px; width: 1167px; margin: 0 auto;">
+                <div class="row">
+                    <div class="col-4 d-flex align-items-center">
+                        <img src="Assets/png/24 1.png" style="width: 355px; height: 240px; margin-left: 15px; margin-bottom: 12px;" class="d-flex align-items-center">
+                    </div>
+                    <div class="col">
+                        <div class="pt-3">
+                            <h1 class="text-artikel">Bersatu dalam Pengetahuan: Cerita di Balik Kegiatan Sosialisasi DPC Persagi Karawang</h1>
+                            <div class="d-flex gap-2">
+                                <p class="txt-mid-art py-2">KARAWANG - Gizi yang seimbang dan berkualitas bukan hanya sekadar tren, tetapi juga merupakan pilar utama dalam menjaga kesehatan dan meningkatkan produktivitas dalam kehidupan sehari-hari. DPC Persagi Karawang dengan tegas mengangkat isu pentingnya gizi bagi anggota dan komunitas dalam upaya mempromosikan kesejahteraan dan kemampuan kerja yang optimal.</p>
+                            </div>
+                            <div class="d-flex justify-content-between py-3 ms-auto">
+                              <p class="txt-foot-art d-flex gap-3 "><img src="Assets/png/Vector (5).png"  style ="width: 26px; height: 23.636px;" alt=""> 21 Februari 2021</p>
+                              <p></p>
+                              <a href="#" class="btn btn-out" style="margin-right: 20px;">Baca Selengkapnya</a>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card d-flex gap-3 card-lg" style="height: 260px; width: 1167px; margin: 0 auto;">
+              <div class="row">
+                  <div class="col-4 d-flex align-items-center">
+                      <img src="Assets/png/24 1.png" style="width: 355px; height: 240px; margin-left: 15px; margin-bottom: 12px;" class="d-flex align-items-center">
+                  </div>
+                  <div class="col">
+                      <div class="pt-3">
+                          <h1 class="text-artikel">Bersatu dalam Pengetahuan: Cerita di Balik Kegiatan Sosialisasi DPC Persagi Karawang</h1>
+                          <div class="d-flex gap-2">
+                              <p class="txt-mid-art py-2">KARAWANG - Gizi yang seimbang dan berkualitas bukan hanya sekadar tren, tetapi juga merupakan pilar utama dalam menjaga kesehatan dan meningkatkan produktivitas dalam kehidupan sehari-hari. DPC Persagi Karawang dengan tegas mengangkat isu pentingnya gizi bagi anggota dan komunitas dalam upaya mempromosikan kesejahteraan dan kemampuan kerja yang optimal.</p>
+                          </div>
+                          <div class="d-flex justify-content-between py-3 ms-auto">
+                            <p class="txt-foot-art d-flex gap-3 "><img src="Assets/png/Vector (5).png"  style ="width: 26px; height: 23.636px;" alt=""> 21 Februari 2021</p>
+                            <p></p>
+                            <a href="#" class="btn btn-out" style="margin-right: 20px;">Baca Selengkapnya</a>
+                        </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+            
+            </div>
+        </div>
+    </section>
+
+    <!--Artikel Lainnya-->
+      <section class=" bg-white">
+      <div class="Artikel d-flex justify-content-center py-5 ">Artikel Lainnya
+      </div>
+      <!-- Caraousel -->
+        <div class="swiper mySwiper py-3">
+          <div class="swiper-wrapper d-flex gap-2 justify-content-center1">
+              <div class="card swiper-slide" style="width: 420px;">
+                <img src="Assets/png/artikel-1 1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-outline-primary">Baca Selengkapnya</a>
+                </div>
+            </div>
+            <div class="card swiper-slide" style="width: 18rem;">
+              <img src="Assets/png/artikel-1 1.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>
+          <div class="card swiper-slide" style="width: 18rem;">
+            <img src="Assets/png/artikel-1 1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card swiper-slide" style="width: 18rem;">
+                <img src="Assets/png/artikel-1 1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          
+          <div class="swiper-pagination"></div>
+        </div>
+      <div>
+        <div>
+          <button class="btn btn-outline-primary btn-art d-flex mx-auto mt-5">
+            Lihat Artikel Lainnya
+          </button>
+        </div>
+      </section>
+      
+
+    
+
+     <!--footer-->
+    <footer class="bg-white text-white text-left pt-3">
+        <div class="container">
+            <div class="footer-content d-flex ">
+                <img src="Assets/png/Logo_Persagi 1.png" alt="Logo Situs" width="50px">
+                <div class="txt-f ms-2">
+                    <p class="mb-0">DPC PERSAGI</p>
+                    <p>KARAWANG</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-main">
+            <div class="container pt-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <p class="py-2">Lorem ipsum dolor sit amet consectetur. Dictumst tristique vitae diam quis
+                            egestas magna feugiat sit vel. Cras vel ut diam facilisi feugiat eu. Et ultrices quis fames
+                            nisi convallis. Tincidunt dictum fermentum tempor pellentesque arcu faucibus at.</p>
+                        <div class="d-flex gap-2">
+                            <p><img src="Assets/png/Vector (3).png" width="18px" alt=""></p>
+                            <p class="txt-foot">Facebook</p>
+                        </div>
+                        <div class="d-flex gap-2">
+                            <p><img src="Assets/png/Vector (4).png" width="18px" alt=""></p>
+                            <p class="txt-foot">Instagram</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt-4 mt-md-0">
+                        <p class="btn-art">Kontak</p>
+                        <div class="d-flex gap-3">
+                            <p><img src="Assets/png/Vector (1).png" alt="" width="18px"></p>
+                            <p>Jalan Parahiyangan No.39, Adiarsa Bar., Kec. Karawang Bar., Karawang, Jawa Barat 41311
+                            </p>
+                        </div>
+                        <div class="d-flex gap-3">
+                            <p><img src="Assets/png/Vector (2).png" alt="" width="18px"></p>
+                            <p>dpcpersagi3215@gmail.com</p>
+                        </div>
+                        <div class="d-flex gap-3">
+                            <p><img src="Assets/png/material-symbols_call.png" alt="" width="18px"></p>
+                            <p>+62852 2620 3601</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt-4 mt-md-0">
+                        <div class="">
+                            <p class="btn-art">Lokasi</p>
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.5878516681496!2d107.30447377411514!3d-6.317743861821297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6977cebb9afa29%3A0x67e83add75c68c4a!2sJl.%20Parahiyangan%20No.39%2C%20Adiarsa%20Bar.%2C%20Kec.%20Karawang%20Bar.%2C%20Karawang%2C%20Jawa%20Barat%2041311!5e0!3m2!1sen!2sid!4v1691586432090!5m2!1sen!2sid"
+                                width="100%" height="255" style="border:0; border-radius: 5px;"
+                                allowfullscreen="" loading="lazy"
+                                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col">
+                        <img src="Assets/png/Line 5.png" alt="" width="100%" class="mb-3">
+                        <p class="text-center">DPC Persagi Karawang, All right reserved</p>
+                        <p class="text-center">Designed and Developed by </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button id="toTopButton" class="hidden to-top-button">
+            <span class="arrow">▲</span>
+        </button>
+    </footer>
+    
+    
+    
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
