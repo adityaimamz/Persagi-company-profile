4af7132ec81b7242601b11afb23b3007e61453db
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-08-22 15:16:02 +0700

    make form status gizi

--- resources/views/status-gizi.blade.php
+++ resources/views/status-gizi.blade.php
@@ -52,43 +52,118 @@
                         <div class="tab-pane fade show active" id="pills-dewasa" role="tabpanel"
                             aria-labelledby="dewasa-tab">
                             <div class="container" style="max-width: 700px">
+                                <h4 class="text-center">Masukan Data</h4>
                                 <form action="{{ route('login.change') }}" method="POST">
                                     @method('PUT')
                                     @csrf
                                     {{-- <input type="hidden" name="id" value="{{ Auth::user()->id }}"> --}}
                                     <div class="row mb-3">
-                                        <label for="newPassword" class="col-md-4 col-lg-3 col-form-label">Password
-                                            Baru</label>
-                                        <div class="col-md-8 col-lg-9">
-                                            <input name="newpassword" type="password" class="form-control" id="newPassword">
+                                        <label for="jenisKelamin" class="col-md-4 col-lg-3 col-form-label">Jenis
+                                            Kelamin</label>
+                                        <div class="col-md-8 col-lg-9 gap-3 pt-2 gap-2">
+                                            <input type="radio" name="jenisKelamin" id="lakiLaki" value="laki-laki">
+                                            <label for="lakiLaki">Laki-Laki</label>
+
+                                            <input type="radio" class="" name="jenisKelamin" id="perempuan"
+                                                value="perempuan">
+                                            <label for="perempuan">Perempuan</label>
                                         </div>
                                     </div>
 
                                     <div class="row mb-3">
-                                        <label for="renewPassword" class="col-md-4 col-lg-3 col-form-label">Ulangi
-                                            Password Baru</label>
+                                        <label for="beratBadan" class="col-md-4 col-lg-3 col-form-label">
+                                            Berat Badan (kg)</label>
                                         <div class="col-md-8 col-lg-9">
-                                            <input name="newpassword_confirmation" type="password" class="form-control"
-                                                id="renewPassword">
+                                            <input name="beratBadan" type="number" class="form-control" id="beratBadan">
                                         </div>
                                     </div>
 
-                                    <div class="text-center">
-                                        <button type="submit" style="width: 250px" class="btn btn-primary">Ubah
-                                            Password</button>
+                                    <div class="row mb-3">
+                                        <label for="tinggiBadan" class="col-md-4 col-lg-3 col-form-label">
+                                            Tinggi Badan (cm)</label>
+                                        <div class="col-md-8 col-lg-9">
+                                            <input name="tinggiBadan" type="number" class="form-control" id="tinggiBadan">
+                                        </div>
+                                    </div>
+                                    <div class="text-center mb-3">
+                                        <button type="submit" class="btn btn-primary w-full">Hitung IMT</button>
                                     </div>
                                 </form>
                             </div>
 
                         </div>
                         <div class="tab-pane fade " id="pills-balita" role="tabpanel" aria-labelledby="balita-tab">
-                            Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non
-                            est
-                            unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit.
-                            Fuga
-                            sequi sed ea saepe at unde.
+                            <div class="container" style="max-width: 700px">
+                                <h4 class="text-center">Masukan Data</h4>
+                                <form action="{{ route('login.change') }}" method="POST">
+                                    @method('PUT')
+                                    @csrf
+                                    {{-- <input type="hidden" name="id" value="{{ Auth::user()->id }}"> --}}
+                                    <div class="row mb-3">
+                                        <label for="jenisKelamin" class="col-md-4 col-lg-3 col-form-label">Jenis
+                                            Kelamin</label>
+                                        <div class="col-md-8 col-lg-9 gap-3 pt-2 gap-2">
+                                            <input type="radio" name="jenisKelamin" id="lakiLaki" value="laki-laki">
+                                            <label for="lakiLaki">Laki-Laki</label>
+
+                                            <input type="radio" class="" name="jenisKelamin" id="perempuan"
+                                                value="perempuan">
+                                            <label for="perempuan">Perempuan</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <label for="tanggalLahir" class="col-md-4 col-lg-3 col-form-label">
+                                            Tanggal Lahir</label>
+                                        <div class="col-md-8 col-lg-9">
+                                            <input name="tanggalLahir" type="date" class="form-control" id="tanggalLahir">
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <label for="tanggalDiukur" class="col-md-4 col-lg-3 col-form-label">
+                                            Tanggal Diukur</label>
+                                        <div class="col-md-8 col-lg-9">
+                                            <input name="tanggalDiukur" type="date" class="form-control" id="tanggalDiukur">
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <label for="usiaDiukur" class="col-md-4 col-lg-3 col-form-label">
+                                            Usia Diukur (Bulan)</label>
+                                        <div class="col-md-8 col-lg-9">
+                                            <input name="usiaDiukur" type="number" class="form-control" id="usiaDiukur">
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <label for="beratBadan" class="col-md-4 col-lg-3 col-form-label">
+                                            Berat Badan (kg)</label>
+                                        <div class="col-md-8 col-lg-9">
+                                            <input name="beratBadan" type="number" class="form-control" id="beratBadan">
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <label for="tinggiBadan" class="col-md-4 col-lg-3 col-form-label">
+                                            Tinggi Badan (cm)</label>
+                                        <div class="col-md-8 col-lg-9">
+                                            <input name="tinggiBadan" type="number" class="form-control" id="tinggiBadan">
+                                        </div>
+                                    </div>
+                                    <div class="text-center mb-3">
+                                        <button type="submit" class="btn btn-primary w-full">Hitung IMT</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div><!-- End Pills Tabs -->
+                </div>
+            </div>
+
+            <div class="card mt-4 card-imt">
+                <div class="card-body">
+                    <h4 class="text-center">Hasil perhitungan</h4>
                 </div>
             </div>
         </div>
