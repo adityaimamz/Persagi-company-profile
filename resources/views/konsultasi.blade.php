861d2cf437216463ad6403a2030fde85288692b8
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-08-23 11:38:06 +0700

    Update konsultasi.blade.php

--- resources/views/konsultasi.blade.php
+++ resources/views/konsultasi.blade.php
@@ -115,7 +115,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="Nama pasien" class="col-md-4 col-lg-3 col-form-label">Keluhan</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" name="keluhan" placeholder="Masukan Deskripsi Keluhan Utama pasien dan jika pasien memiliki riwayat penyakit harap dicantumkan "></textarea>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" name="keluhan" placeholder="Masukan deskripsi keluhan utama pasien dan jika pasien memiliki riwayat penyakit harap dicantumkan "></textarea>
                 </div>
                 <div class="mb-5">
                     <button type="submit" class="btn btn-primary">Kirim</button>
