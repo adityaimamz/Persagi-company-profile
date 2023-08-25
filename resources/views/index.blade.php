f2b230dc3a59a00604455cff789600189b06e084
adnan <adnanega82@gmail.com>
2023-08-25 19:38:15 +0700

    [add] vanue event

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -90,7 +90,7 @@
                         <div class="card swiper-slide d-flex gap-3 card-lg">
                             <div class="row">
                                 <div class="col-4">
-                                    <img src="http://localhost:8001/storage/{{ $event->gambar }}" class=""
+                                    <img src="http://localhost:8000/storage/{{ $event->gambar }}" class=""
                                         alt="...">
                                 </div>
                                 <div class="col">
@@ -109,7 +109,7 @@
                                         <div class="d-flex gap-1">
                                             <p><img src="Assets/png/fluent_location-16-filled.png" width="18px"
                                                     alt=""></p>
-                                            <p class="txt-foot"> Online (Zoom Meeting)
+                                            <p class="txt-foot"> {{ $event->vanue }}
                                             </p>
                                         </div>
                                         <a href="" class="btn btn-light text-main border-main mb-3 me-auto ">Baca
