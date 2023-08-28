170a82f699d15f4db12cd762c62bbe747fd9bb48
adnan <adnanega82@gmail.com>
2023-08-29 00:36:46 +0700

    [feature] perhitungan balita

---
+++ app/Models/Lhfa_girls.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lhfa_girls extends Model
+{
+    use HasFactory;
+    protected $table = 'lhfa_girls';
+}
---
+++ app/Models/Wfl_girls.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wfl_girls extends Model
+{
+    use HasFactory;
+    protected $table = 'wfl_girls';
+}
